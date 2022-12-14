b0fdcbe82e46b796c2af3e8cfc2b1b779dfc845f
Varun <varunbabu008@gmail.com>
2022-12-14 18:28:13 +0530

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "varunbabu008-org"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
